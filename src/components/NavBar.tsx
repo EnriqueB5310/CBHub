7feda9834dceca502e5a8ab9a03798fdf8547954
EnriqueB5310 <124002775+EnriqueB5310@users.noreply.github.com>
2024-11-27 14:17:21 -0500

    installed axios

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -34,7 +34,7 @@ function NavBar() {
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                >
+                >  
                   <line x1="18" y1="6" x2="6" y2="18" />
                   <line x1="6" y1="6" x2="18" y2="18" />
                 </svg>
--- src/routes/Releases.tsx
+++ src/routes/Releases.tsx
@@ -4,7 +4,7 @@ import NavBar from '../components/NavBar'
 function Releases() {
   return (
     
-    <NavBar />
+    <NavBar />   
   )
 }
 
